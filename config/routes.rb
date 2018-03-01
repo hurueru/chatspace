b7a1ab09a2118d90b2298bf1a9fc33021522c175
hurueru <tech-camp@12-010tech-camp-EXPtaikenkai1-A-no-MacBook-Air.local>
2018-03-01 11:49:42 +0900

    frist commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # チャット画面作成のために仮置きした。あとで編集する。
+  root  'messages#index'
+  get 'messages' => 'messages#index'
 end
