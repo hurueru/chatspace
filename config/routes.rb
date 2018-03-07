d94ed219e634adf6c25edbe9ffc24feb4358e6c1
hurueru <tech-camp@12-010tech-camp-EXPtaikenkai1-A-no-MacBook-Air.local>
2018-03-07 10:40:43 +0900

    commit8 user_function changed config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # チャット画面作成のために仮置きした。あとで編集する。
   root  'messages#index'
-  get 'messages' => 'messages#index'
+  resource :users, only: [:index, :edit, :update]
+  resource :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
